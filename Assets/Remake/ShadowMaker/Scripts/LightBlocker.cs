61e1c3f57dfcddddcc9fb55a6b1bc9fad14e73c0
ivarus <ivarbjorling@hotmail.com>
2019-01-23 22:47:47 +0100

    StyleCop

--- Assets/Remake/ShadowMaker/Scripts/LightBlocker.cs
+++ Assets/Remake/ShadowMaker/Scripts/LightBlocker.cs
@@ -1,47 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace ShadowMaker
+﻿namespace ShadowMaker
 {
-    using ShadowMaker.Core;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
+    /// <summary>
+    /// Component which blocks the light of a light emitter.
+    /// </summary>
     [RequireComponent(typeof(MeshFilter))]
     [RequireComponent(typeof(MeshRenderer))]
     public class LightBlocker : MonoBehaviour
     {
+        /// <summary>
+        /// Static list of all the active blockers in the scene.
+        /// </summary>
         private static List<LightBlocker> blockerList = new List<LightBlocker>();
 
-        private LightBlockerMesh lightBlockerMesh = null;
+        /// <summary>
+        /// The light blocker mesh.
+        /// </summary>
+        private ShadowMaker.Core.LightBlockerMesh lightBlockerMesh = null;
 
+        /// <summary>
+        /// Gets the list of active blockers.
+        /// </summary>
+        /// <returns>The list of active blockers.</returns>
         public static List<LightBlocker> GetActiveBlockerList()
         {
             return LightBlocker.blockerList;
         }
 
-        private static void AddBlocker(LightBlocker blocker)
-        {
-            Debug.Assert(!LightBlocker.blockerList.Contains(blocker), typeof(LightBlocker).FullName + ".AddBlocker: Already in list.");
-            LightBlocker.blockerList.Add(blocker);
-        }
-        
-        private static void RemoveBlocker(LightBlocker blocker)
-        {
-            Debug.Assert(LightBlocker.blockerList.Contains(blocker), typeof(LightBlocker).FullName + ".RemoveBlocker: Not in list.");
-            LightBlocker.blockerList.Remove(blocker);
-        }
-
+        /// <summary>
+        /// Gets the render mesh.
+        /// </summary>
+        /// <returns>The render mesh.</returns>
         public Mesh GetRenderMesh()
         {
             return this.gameObject.GetComponent<MeshFilter>().sharedMesh;
         }
 
+        /// <summary>
+        /// Gets the blocker mesh (used when rendering shadow map).
+        /// The blocker is generated if it does not exist or if the render mesh is changed.
+        /// </summary>
+        /// <returns>The blocker mesh.</returns>
         public Mesh GetBlockerMesh()
         {
             // If blocker never has been generated.
             if (this.lightBlockerMesh == null)
             {
-                this.lightBlockerMesh = new LightBlockerMesh(this.GetRenderMesh());
+                this.lightBlockerMesh = new Core.LightBlockerMesh(this.GetRenderMesh());
             }
 
             // If render mesh has been changed, blocker mesh needs to be updated.
@@ -53,22 +61,42 @@ namespace ShadowMaker
             return this.lightBlockerMesh.GetBlockerMesh();
         }
 
-        private void Awake()
+        /// <summary>
+        /// Adds a blocker to the active blocker list.
+        /// </summary>
+        /// <param name="blocker">The blocker to add.</param>
+        private static void AddBlocker(LightBlocker blocker)
         {
+            Debug.Assert(blocker.gameObject.activeInHierarchy, typeof(LightBlocker).FullName + ".AddBlocker: Blocker is not active.");
+            Debug.Assert(!LightBlocker.blockerList.Contains(blocker), typeof(LightBlocker).FullName + ".AddBlocker: Already in list.");
+            LightBlocker.blockerList.Add(blocker);
         }
 
+        /// <summary>
+        /// Removes a blocker from the active blocker list.
+        /// </summary>
+        /// <param name="blocker">The blocker to remove.</param>
+        private static void RemoveBlocker(LightBlocker blocker)
+        {
+            Debug.Assert(!blocker.gameObject.activeInHierarchy, typeof(LightBlocker).FullName + ".AddBlocker: Blocker is active.");
+            Debug.Assert(LightBlocker.blockerList.Contains(blocker), typeof(LightBlocker).FullName + ".RemoveBlocker: Not in list.");
+            LightBlocker.blockerList.Remove(blocker);
+        }
+
+        /// <summary>
+        /// Unity method called when game object is enabled.
+        /// </summary>
         private void OnEnable()
         {
             LightBlocker.AddBlocker(this);
         }
 
+        /// <summary>
+        /// Unity method called when game object is disabled.
+        /// </summary>
         private void OnDisable()
         {
             LightBlocker.RemoveBlocker(this);
-        }
-
-        private void OnDestroy()
-        {
         }
     }
 }
